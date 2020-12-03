8e50d8bde4b59a7de69464a24e4687842cd7e304
EVANGELOS TZIMAGIORGIS <evangelostzimagiorgis@gmail.com>
2020-12-04 00:29:26 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const clock = document.querySelector(".clock");
+
+const time = () => {
+    const time = new Date();
+
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+
+    if(hours < 10){
+        hours = "0" + hours;
+    }
+
+    if(minutes < 10){
+        minutes = "0" + minutes;
+    }
+
+    if(seconds < 10){
+        seconds = "0" + seconds;
+    }
+
+    clock.innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+
+setInterval(time, 1000);
